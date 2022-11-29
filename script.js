a03855644083ba0a22a283f6f875a6eb83e8f250
KanimozhiSivakumar <118879210+KanimozhiSivakumar@users.noreply.github.com>
2022-11-29 22:27:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var odd = (arr)=>{
+    var result =[];
+        for(var i=0;i<arr.length;i++){
+            if(arr[i]%2!=0){
+               result.push(arr[i]);
+            }
+        }
+        return result;
+}
+console.log(odd([12,13,14,15,17,19,21]))
+
+
